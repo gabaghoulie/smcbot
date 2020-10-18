ae9a5b5a05305e723bd7253154bafea2f00a6a67
burnsnoss <burnsnoss@wowbagger.dreamhost.com>
2020-10-17 20:17:03 -0700

    asdf

--- bot.js
+++ bot.js
@@ -1,14 +1,13 @@
 const tmi = require('tmi.js');
-let bukkake = 0;
 
 // Define configuration options
 const opts = {
   identity: {
-    username: 'SausageMcBot',
+    username: 'sausagemcbot',
     password: process.env.OAUTH_TOKEN
   },
   channels: [
-    'SausageMcBurn'
+    'sausagemcburn'
   ]
 };
 
@@ -22,19 +21,41 @@ client.on('connected', onConnectedHandler);
 // Connect to Twitch:
 client.connect();
 
+// globals
+let bukkake = 0;
+let quotes = ["Elbow pasta is the Milhouse Van Houten of pasta",
+"ive arrived clown boys and squirter girls",
+"ive returned and im sad horny",
+"mi pachoochie es juithy",
+"step on me steve harrington", 
+"my mom is blue balling me with her lasagna",
+"im boutta squirt some sauce in a minute", 
+"horny typing is a lot of work my brain barely Functioning", 
+"want glencour to raw me out goodnigjt", 
+"wish xqc would pounce on this xpussc", 
+"i sucked a shelob before", 
+"threats are hot tho", 
+"play with me be my gacha sugar daddy", 
+"typing and stealing my grandma's chicken wings rn", 
+"oh to be a dumpster...", 
+"need dj mans to fill my hol- I mean... the void in my as--heart", 
+"time to start peppegging his dad", 
+"go use ur douching toilet beta boy @NuggiesBoy",
+"boutta astral project my juicy pussy to the perfect dimension"];
+
 // Called every time a message comes in
 function onMessageHandler (target, context, msg, self) {
   if (self) { return; } // Ignore messages from the bot
-  
+  // console.log(context); 
   msg = msg.toLowerCase();
-  
-  if (msg.includes('bukkake')) {
+
+  if (msg.includes('bukkake') && target != '#sausagemcbot') {
     bukkake += 1;
     client.say(target, `ah i see you're a man of culture as well :: bukkake count = ${bukkake}`);
     return;
   }
   
-  if (msg.includes('chosen one')) {
+  if (msg.includes('chosen one') && target != '#sausagemcbot') {
     bukkake += 1;
     client.say(target, ` ..(‿ˠ‿) _(‿ˠ‿) only the chosen one can fist both asses.⎝. Kreygasm .⎠`);
     return;
@@ -47,6 +68,11 @@ function onMessageHandler (target, context, msg, self) {
   if (commandName === '!d20') {
     const num = rollDice(commandName);
     client.say(target, `You rolled a ${num}`);
+    return;
+  }
+  if (commandName === '!puchi') {
+    ribbon = String.fromCodePoint(0x1F380);
+    client.say(target, `${ribbon} ${quotes[Math.floor(Math.random()*quotes.length)]} ${ribbon}`);
     return;
   }
 }
