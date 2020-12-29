ea8ea65b51a279d09e65dfe171b6aacec1e5e570
Casey Burns <caseyburns33@gmail.com>
2020-12-28 19:29:08 -0800

    adding bigfollows logic

--- bot.js
+++ bot.js
@@ -1,4 +1,11 @@
 const tmi = require('tmi.js');
+var jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const { window } = new JSDOM();
+const { document } = (new JSDOM('')).window;
+global.document = document;
+
+var $ = jQuery = require('jquery')(window);
 
 // Define configuration options
 const opts = {
@@ -10,6 +17,8 @@ const opts = {
     'sausagemcburn'
   ]
 };
+
+const smb_id = '219092110';
 
 // Create a client with our options
 const client = new tmi.client(opts);
@@ -37,11 +46,33 @@ function onMessageHandler (target, context, msg, self) {
     client.say(target, `ah i see you're a man of culture as well :: bukkake count = ${bukkake}`);
     return;
   }
+
+  if (msg.includes('bigswallows') && target != '#sausagemcbot') {
+    client.say(target, `are you really watching porn alone?? check out bigswallows . dot com (;; click here to see my pussy`);
+    return;
+  }
   
   if (msg.includes('chosen one') && target != '#sausagemcbot') {
-    bukkake += 1;
     client.say(target, ` ..(‿ˠ‿) _(‿ˠ‿) only the chosen one can fist both asses.⎝. Kreygasm .⎠`);
     return;
+  }
+
+  if (msg.includes('bigfollows')) {
+    // use twitch API to check if they're a follower
+    $.ajax({
+      type: 'GET',
+      url: `https://api.twitch.tv/helix/users/follows?from_id=${context['user-id']}&to_id=${smb_id}`,
+      headers: {
+        'Client-ID': process.env.TTV_CLIENT_ID,
+        'Authorization': `Bearer ${process.env.ACCESS_TOKEN}`
+      },
+      success: function(data) {
+        // time 'em out if they dont follow (bad boys bad boys)
+        if (data.total === 0) {
+          client.say(target, `/timeout ${context['display-name']} 300`);
+        }
+      }
+    });
   }
 
   // Remove whitespace from chat message
