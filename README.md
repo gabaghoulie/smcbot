890fa42e4a957bae99f70e039d89d9a77ed76729
Casey Burns <caseyburns33@gmail.com>
2021-06-24 08:39:08 -0700

    adding quick stonk test API

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # sausagemcbot
 chatbot for sausagemcburn's stream on twitch
+
+TODO
+====
+  - cron job that detects when Sausage is live and starts bot
+    - python script?
+    - can just go out to the twitch API 
+    - how to keep track of bot PID (to determine if it's running)
+      - write PID to a file? search for PID using ps -aux?
+  - start commercial
+  - custom sound alerts + corresponding alert browser source
--- bot.js
+++ bot.js
@@ -126,6 +126,31 @@ function onMessageHandler (target, context, msg, self) {
   if (commandName === '!recipe') {
     client.say(target, `Spicy Sausage Rigatoni - https://www.pinchofyum.com/spicy-sausage-rigatoni `);
   }
+  if (commandName === '!stonks') {
+
+    var request = require('request');
+
+    // replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+    var url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=ATOS&interval=5min&apikey=demo';  
+
+    request.get({
+        url: url,
+        json: true,
+        headers: {'User-Agent': 'request'}
+      }, (err, res, data) => {
+        if (err) {
+          console.log('Error:', err);
+        } else if (res.statusCode !== 200) {
+          console.log('Status:', res.statusCode);
+        } else {
+          // data is successfully parsed as a JSON object:
+          console.log(data);
+        }
+    });
+
+    //client.say(target, ``);
+  }
+
 
 
   // CHECKING SPLIT MESSAGE
