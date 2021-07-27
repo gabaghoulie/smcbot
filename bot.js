0f0b4199610889eb8901219a90006e130c511bf6
Sausage McBurn <sausagemcburn.tv@gmail.com>
2021-07-27 16:26:05 -0700

    fixing typo

--- bot.js
+++ bot.js
@@ -252,7 +252,7 @@ function topPuchisms() {
   });
   // print top 3 puchisms
   let output = "";
-  for (let i = 0; i < 3, i++) {
+  for (let i = 0; i < 3; i++) {
     output += sortable[i][0] + ": " + sortable[i][1] + " || ";
   }
   return output;
